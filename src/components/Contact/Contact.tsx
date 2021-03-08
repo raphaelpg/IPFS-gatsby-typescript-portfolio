b509e901d5b06e4bf67e828b2ba4ed4fa58ff2a8
raphaelpg <raphael_pinto@outlook.com>
2021-03-08 14:01:15 +0100

    add type checking in contact form

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,25 +1,40 @@
 import React, { useState } from 'react'
 
+interface FormValues {
+  name: {value: string}
+  email: {value: string}
+  message: {value: string}
+}
+
 const Contact: React.FC = () => {
   const [status, setStatus] = useState<string>('')
 
-  const submitForm = (e: any) => {
+  const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const form = e.target;
-    const data = new FormData(form);
-    const xhr = new XMLHttpRequest();
-    xhr.open(form.method, form.action);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        form.reset();
-        setStatus("SUCCESS")
-      } else {
-        setStatus("ERROR")
-      }
-    };
-    xhr.send(data);
+    const form = (e.target as HTMLFormElement);
+
+    const target: FormValues = e.target as typeof e.target & {
+      name: {value: string}
+      email: {value: string}
+      message: {value: string}
+    }
+    
+    if(typeof target.name.value === 'string' && typeof target.email.value === 'string' && typeof target.message.value === 'string') {
+      const data = new FormData(form);
+      const xhr = new XMLHttpRequest();
+      xhr.open(form.method, form.action);
+      xhr.setRequestHeader("Accept", "application/json");
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+          form.reset();
+          setStatus("SUCCESS")
+        } else {
+          setStatus("ERROR")
+        }
+      };
+      xhr.send(data);
+    }
   }
 
   return (
